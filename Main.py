a2a9254d34e68e88728e84caac96505fc5e46f5c
Gustavo Melo <gtvmelo10@gmail.com>
2020-10-14 19:12:22 -0300

    Terminado a opção 1 e os nomes das listas foram mudados por boas praticas.

--- Main.py
+++ Main.py
@@ -1,7 +1,17 @@
-Vendedores = []
-Produtos = []
-Vendas = []
+from Classes import Vendedor
+from Classes import Produto
+from Classes import Venda
 
+vendedores = []
+produtos = []
+vendas = []
+
+def validarCadastro(cd, lista):
+    for i in lista:
+        if(i.cd == cd):
+            return False
+    if(cd < 0):
+        return False
 while(True):
     opcao = int(input("Digite a opção desejada:\n"
                       "1 - Cadastrar Vendedores\n"
@@ -10,8 +20,18 @@ while(True):
                       "4 - Listar Produtos vendidos por Vendedor\n"
                       "5 - Sair\n"))
     if(opcao == 1):
-        
-
+        if(len(vendedores) > 0):
+            print("Você ja cadastrou os vendedores. Voltando para o menu...")
+        cont = 0
+        while cont < 5:
+            cd = int(input(f"({cont + 1}) - Digite o codigo do Vendedor:"))
+            if(validarCadastro(cd, vendedores) == False):
+                print("O codigo já está cadastrado ou é menor que 0. Tente novamente.")
+                continue
+            nome = input("Digite o nome do vendedor:")
+            vendedor = Vendedor(cd, nome)
+            vendedores.append(vendedor)
+            cont+=1
     elif(opcao == 5):
         print("Programa finalizado!")
         break
